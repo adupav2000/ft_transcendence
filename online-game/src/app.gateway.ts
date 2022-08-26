231398c8a047e589e783b319c47eed3a1edceaa7
Baptiste DITTE <bditte@e1r12p3.clusters.42paris.fr>
2022-08-27 00:09:29 +0200

    back update for game

--- online-game/src/app.gateway.ts
+++ online-game/src/app.gateway.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { string } from 'yargs';
 
 // const options = {
 // 	handlePreflightRequest: (req, res) => {
@@ -17,32 +18,84 @@ import { Socket, Server } from 'socket.io';
 // 	},
 //   }
 
+interface Player { id: string, pos: number}
+
 @WebSocketGateway(8001, { cors: '*' })
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-	
+
+	players: Player[] = [];
+	stateChanged = false;
+	isEmittingUpdates = false;
+
 	@WebSocketServer()
 	server;
 
 	private logger: Logger = new Logger('AppGateway');
 
+	createClient(id: string) {
+		return {
+			id: id,
+			pos: 0,
+		}
+	}
+
+	emitUpdates() {
+		this.isEmittingUpdates = true;
+
+		if (this.stateChanged)
+		{
+			this.stateChanged = false;
+			this.server.emit('stateUpdate', this.players);
+		}
+
+		if (this.players?.length > 0)
+			setTimeout(this.emitUpdates, 300);
+		else
+			this.isEmittingUpdates = false;
+	}
+
 	afterInit(server: Server) {
 		this.logger.log("Socket initizialed")
+
+		if (this.players?.length == 1 && !this.isEmittingUpdates)
+			this.emitUpdates();
 	}
 
 	handleConnection(client: Socket){
-		this.server.emit("clientConnection");
-		this.logger.log(`Client connected: ${client.id}`)
+		console.log(`Client ${client.id} joined server`);
+		let newPlayer: Player = {id: client.id, pos: 50};
+		//console.log(newPlayer);
+		this.players?.push(newPlayer);
+		/*
+		for (let i = 0; i < this.players.length; i++)
+		{
+			console.log(this.players[i]);
+		}
+		*/
+		
+		//this.server.emit("clientConnection");
+		//this.logger.log(`Client connected: ${client.id}`)
 	}
 
 	handleDisconnect(client: Socket) {
-		this.server.emit("clientDisconnected");
-		this.logger.log(`Client disconnected: ${client.id}`)
+		console.log(`Client ${client.id} left server`);
+		
+		//remove player from players[]
+
+		//this.server.emit("clientDisconnected");
+		//this.logger.log(`Client disconnected: ${client.id}`)
 	}
 
 	@SubscribeMessage('positionChanged')
-	handlePositionChanged(client: Socket, position: number) {
-		client.broadcast.emit('positionChanged', position);
+	handlePositionChanged(client: Socket, data: { id: string, position: number}) {
+		this.stateChanged = true;
+		for (let i = 0; i < this.players.length; i++)
+		{
+			if (this.players[i].id == data.id)
+				this.players[i].pos = data.position; 
+		}
+		//client.broadcast.emit('positionChanged', position);		
 	}
 
 	@SubscribeMessage('msgToServer')
