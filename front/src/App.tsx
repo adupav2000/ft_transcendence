04988e151e5fded76492efffb70dc09cb384eeb1
Kwame AMANFO <kamanfo@e1r12p1.clusters.42paris.fr>
2022-08-26 22:09:48 +0200

    front test socket position

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,12 @@
 import { useEffect, useState } from 'react'
-import { io, Socket } from 'socket.io-client'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Pong from './game/Components/Pong/pong'
 
 function App() {
-  const [socket, setSocket] = useState<Socket>()
-
-  const sendMessage = () => {
-    socket?.emit("msgToServer", "chaud");
-    console.log("Sending chaud");
-  }
-
-  useEffect(() => {
-    const newSocket = io("http://localhost:8001");
-    setSocket(newSocket);
-  }, [setSocket])
-
-  useEffect(() => {
-    socket?.on("msgToClient", (msg) => {
-      console.log(`Received ${msg}`);
-    })
-
-  })
 
   return (
     <div className="App">
-      <button style={{
-        height: "300px",
-        width: "300px"
-      }} onClick={sendMessage}></button>
-
+		<Pong/>
     </div>
   );
 }
